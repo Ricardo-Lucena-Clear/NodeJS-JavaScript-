87c953a822941701fbca180eafe2f2cc4f4c99d6
Ricardo Lucena <ricardo.lucena.clear@gmail.com>
2023-03-10 09:15:49 -0300

    Data persistence with NeDB (JavaScript Database)

--- routes/users.js
+++ routes/users.js
@@ -1,3 +1,10 @@
+     let NeDB = require ('nedb');
+     let db = new NeDB({
+         filename:'users.db',
+         autoload:true
+     })
+     
+     
      module.exports =app =>{
         app.get('/users', (req, res)=>{
             res.statusCode = 200;
@@ -11,8 +18,16 @@
                 })
             });
             app.post('/users', (req, res)=>{
-                
-                    res.json(req.body);
+                    db.insert(req.body, (err,user)=>{
+                        if (err){
+                            console.log('error: ${err}');
+                            res.status(400).json({
+                                error: err
+                            });                            
+                        }else {
+                            res.status(200).json(user);
+                        }
+                    })
                     
                 });
         
