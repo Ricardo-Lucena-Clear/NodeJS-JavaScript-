6335850f667a8735c42d1acd860dc6f24ab8cc30
Ricardo Lucena <ricardo.lucena.clear@gmail.com>
2023-03-10 08:44:13 -0300

    Loading Routes with Consign

--- routes/users.js
+++ routes/users.js
@@ -1,24 +1,22 @@
-let express = require('express');
-let routes = express.Router();
-
-
-routes.get('/', (req, res)=>{
-    res.statusCode = 200;
-        res.setHeader('Content-Type', 'application/js');
-        res.json({
-            users:[{
-                name:'Ricardo',
-                email:'ricardo@gmail.com',
-                id:1
-            }]
-        })
-    });
-    routes.get('/admin', (req, res)=>{
-        res.statusCode = 200;
-            res.setHeader('Content-Type', 'application/js');
-            res.json({
-                users:[]
+     module.exports =app =>{
+        app.get('/users', (req, res)=>{
+            res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/js');
+                res.json({
+                    users:[{
+                        name:'Ricardo',
+                        email:'ricardo@gmail.com',
+                        id:1
+                    }]
+                })
             });
-        });
+            app.get('/users/admin', (req, res)=>{
+                res.statusCode = 200;
+                    res.setHeader('Content-Type', 'application/js');
+                    res.json({
+                        users:[]
+                    });
+                });
+        
 
-     module.exports = routes;
+     }
